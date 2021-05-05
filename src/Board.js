4b1c629d30f1437a8b2e7a1ed2383c763b8de299
Vy Tran <vtran1022@Gmail.com>
2021-05-05 12:43:25 -0700

    Implementing changes to two helper functions for Rows in solo branch

--- src/Board.js
+++ src/Board.js
@@ -62,7 +62,7 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
@@ -77,15 +77,66 @@
     // ROWS - run from left to right
     // --------------------------------------------------------------
     //
-    // test if a specific row on this board contains a conflict
+
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      // create a current row variable
+      var currentRow = this.get(rowIndex); //this is the board that we're pulling the row from, used rows function in board visual to find this
+      // create a counter variable
+      var counter = 0; // starting with belief that there is no conflicts
+      // iterate over that row variable
+      for (var i = 0; i < currentRow.length; i++) {
+      // if the current row is equal to 1
+        // add one to the counter variable
+        if (currentRow[i] === 1) {
+          counter++;
+        }
+      }
+      // After done iterating, if the counter variable is greater than zero, return true otherwise return false
+      // cannot be zero b/c when entire board visual light up, so that means this is always true.. the first if (row[i]) is the placement! So counter must be greater than 1 b/c it'll check it afterwards
+      if (counter > 1) {
+        return true;
+      } else {
+        return false;
+      }
     },
 
-    // test if any rows on this board contain conflicts
+    /*
+    justification: test if a specific row on this board contains a conflict
+    i: rowIndex - an integar that represents which square within the row you're on
+    o: a boolean that indicates if there are row conflicts or not
+    c: none
+    e: none
+
+    - when changing boolean to 'true', entire board lights up
+    - this is testing one row at a time, if there is a row conflict at a square, we want to state true but we should also keep track of it since we're testing each square in the ENTIRE row
+    */
+
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      // create a board variable -- row function in the board visual
+      var board = this.rows();
+
+      // iterate over that board variable
+      for (var i = 0; i < board.length; i++) {
+      // check if there is a conflict using the other helper function hasRowConflict
+        if (this.hasRowConflictAt(i)) {
+        // if there is then return true,
+          return true;
+        }
+      }
+      // return false outside the for-loop
+      return false;
     },
+
+    /*
+    justification: test if any rows on this board contain conflicts
+    i: no inputs
+    o: a boolean that indicates if there are any row conflicts or not
+    c: none
+    e: none
+
+    - when changing boolean to 'true', board does not light up. Can this work in conjuction with hasRowConflictAt func? b/c if there is a conflict ANYWHERE, then true.
+    - this is testing the entire board's rows, so checking each square within each row, if a square has a conflict, we want to state true, since we're testing if conflict anywhere in the board. The hasRowConflictAt is already checking the entire row, should use somehow
+    */
 
 
 
@@ -97,11 +148,18 @@
       return false; // fixme
     },
 
+    /*
+    - when changing boolean to 'true', entire board lights up
+    */
+
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
       return false; // fixme
     },
 
+    /*
+    - when changing boolean to 'true', board does not light up. Can this work in conjuction with hasColConflictAt func? b/c if there is a conflict ANYWHERE, then true.
+    */
 
 
     // Major Diagonals - go from top-left to bottom-right
@@ -111,13 +169,18 @@
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
       return false; // fixme
     },
+    /*
+    - when changing boolean to 'true', entire board lights up
+    */
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
       return false; // fixme
     },
 
-
+    /*
+    - when changing boolean to 'true', board does not light up. Can this work in conjuction with hasRowConflictAt func? b/c if there is a conflict ANYWHERE, then true.
+    */
 
     // Minor Diagonals - go from top-right to bottom-left
     // --------------------------------------------------------------
@@ -127,10 +190,18 @@
       return false; // fixme
     },
 
+    /*
+    - when changing boolean to 'true', entire board lights up
+    */
+
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
       return false; // fixme
     }
+
+    /*
+    - when changing boolean to 'true', board does not light up. Can this work in conjuction with hasRowConflictAt func? b/c if there is a conflict ANYWHERE, then true.
+    */
 
     /*--------------------  End of Helper Functions  ---------------------*/
 
